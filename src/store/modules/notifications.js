9408ddf860ba402ca93e94163bd20b7671d25b79
Virtue Xolutions <tahirsandh78628@gmail.com>
2023-07-11 21:25:34 +0500

    first commit

---
+++ src/store/modules/notifications.js
@@ -0,0 +1,19 @@
+
+
+
+import { currentUser, isAuthGuardActive,apiUrl,firebaseConfig } from '../../constants/config'
+import Vue from 'vue';
+import axios from 'axios';
+
+export default {
+  state: {
+    notifications: []
+  },
+  getters: {
+
+  },
+  mutations: vuexfireMutations,
+  actions: {
+   
+  }
+}
---
+++ src/views/Comingsoon.vue
@@ -0,0 +1,53 @@
+   <template>
+    
+      <div class="commingsoon">
+          <div v-if="device <= 767">
+           <a href="/">
+            <img src="@/assets/landing-page/mobile-comingsoon-image.jpg" class="img-fluid"/>
+           </a>
+          </div>
+          <div v-else-if="device >= 768  &&  device <= 1024 ">
+            <a href="/">
+            <img src="@/assets/landing-page/ipad-mini-comingsoon-image.jpg" class="img-fluid"/>
+            </a>
+          </div>
+      </div>
+   </template>
+  <script>
+  import AppLayout from "@/layouts/AppLayout";
+  import { mapGetters } from "vuex";
+  export default {
+
+    data() {
+        return {
+          device : null,
+        };
+    },
+    computed: {
+      ...mapGetters(["currentUser"]),
+    },
+    methods: {
+      openModal(id) {
+        this.$bvModal.show(id);
+      },
+      closeModal(id) {
+        this.$bvModal.hide(id);
+      },
+    },
+    mounted() {
+        this.device = screen.width
+    },
+  };
+  </script>
+  
+  <style scoped lang="scss">
+  #app {
+    height: 100%;
+  }
+  .commingsoon img
+  {
+    height: 100vh;
+    width: 100%;
+  }
+  </style>
+  
